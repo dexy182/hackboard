f41bb1528d417fad009d2d13449ffa2ca81ff5c5
Michelle Pomeroy <michelle.pomeroy@hotmail.com>
2013-03-04 20:42:29 -0500

    Create new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,18 @@ class PostsController < ApplicationController
 	def index
 		@posts = Post.all
 	end
+
+	def new
+		@posts = Post.new
+	end
+
+	def create
+		@posts = Post.new(params[:post])
+		if @posts.valid?
+			@posts.save
+			redirect_to posts_path
+		else
+			render :new
+		end
+	end
 end

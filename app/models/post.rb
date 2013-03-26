781f0bce128aabbcde1858a7eb5f340bb166087d
Michelle Pomeroy <michelle.pomeroy@hotmail.com>
2013-03-25 21:17:02 -0400

    Adding Images

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,20 @@
 class Post < ActiveRecord::Base
-  attr_accessible :description, :link
+  attr_accessible :description, :link, :image
 
   validates :description, presence: true
   validates :link, presence: true,
   					format: /http:\/\/./
 
+  validates_attachment :image, :presence => true, :size => { :in => 0..1.megabytes }
+
   has_many :comments
   belongs_to :user
+
+  has_attached_file :image,
+  	styles:{
+  		thumb: '100x100#',
+  		medium: '150x150>',
+  		large: '200x200>'
+  	}
+
 end
